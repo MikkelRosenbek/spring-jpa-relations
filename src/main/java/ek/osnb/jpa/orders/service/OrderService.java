a71cb41dded086ebd24f43927532fa5d58eb86b0
MikkelRosenbek <miro0002@stud.kea.dk>
2025-08-30 16:19:09 +0200

    Completed exercise 05

--- src/main/java/ek/osnb/jpa/orders/service/OrderService.java
+++ src/main/java/ek/osnb/jpa/orders/service/OrderService.java
@@ -2,6 +2,7 @@ package ek.osnb.jpa.orders.service;
 
 
 import ek.osnb.jpa.orders.dto.OrderDto;
+import ek.osnb.jpa.orders.dto.OrderLineDto;
 import ek.osnb.jpa.orders.model.Order;
 import ek.osnb.jpa.orders.model.OrderStatus;
 
@@ -13,4 +14,15 @@ public interface OrderService {
     OrderDto createOrder(OrderDto orderDto);
     OrderDto updateOrder(Long id, OrderDto orderDto);
     void deleteOrder(Long id);
+
+    //void addOrderLine(Long orderId, OrderLineDto orderLineDto);
+    OrderDto addOrderLine(Long orderId, OrderLineDto orderLineDto);
+
+    //void removeOrderLine(Long orderId, Long orderLineId);
+    OrderDto removeOrderLine(Long orderId, Long orderLineId);
+
+
+
+
+
 }
--- src/main/java/ek/osnb/jpa/orders/service/OrderServiceImpl.java
+++ src/main/java/ek/osnb/jpa/orders/service/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package ek.osnb.jpa.orders.service;
 
 import ek.osnb.jpa.orders.dto.OrderDto;
+import ek.osnb.jpa.orders.dto.OrderLineDto;
 import ek.osnb.jpa.orders.dto.OrderMapper;
 import ek.osnb.jpa.orders.model.Order;
+import ek.osnb.jpa.orders.model.OrderLine;
 import ek.osnb.jpa.orders.model.OrderStatus;
 import ek.osnb.jpa.orders.repository.OrderLineRepository;
 import ek.osnb.jpa.orders.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -74,6 +77,45 @@ public class OrderServiceImpl implements OrderService{
         } else {
             throw new RuntimeException("Order not found with id: " + id);
         }
+    }
+
+
+
+    @Override
+    public OrderDto addOrderLine(Long id, OrderLineDto orderLineDto) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isEmpty()) {
+            throw new RuntimeException("Order not found with id: " + id);
+        }
+        Order existingOrder = order.get();
+        var line = OrderMapper.toEntity(orderLineDto);
+        existingOrder.addOrderLine(line);
+        return OrderMapper.toDto(orderRepository.save(existingOrder));
+
+    }
+
+    @Override
+    public OrderDto removeOrderLine(Long id, Long orderLineId) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isEmpty()) {
+            throw new RuntimeException("Order not found with id: " + id);
+        }
+        Order existingOrder = order.get();
+        OrderLine lineToRemove = null;
+
+        for (var line : existingOrder.getOrderLines()) {
+            if (line.getId().equals(orderLineId)) {
+                lineToRemove = line;
+                break;
+            }
+        }
+        if (lineToRemove == null) {
+            throw new RuntimeException(
+                    "OrderLine not found with id: " + orderLineId + " in order " + id
+            );
+        }
+        existingOrder.removeOrderLine(lineToRemove);
+        return OrderMapper.toDto(orderRepository.save(existingOrder));
     }
 
 }
